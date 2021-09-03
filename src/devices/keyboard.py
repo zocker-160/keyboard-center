2cebef3351c5d3c26b48290aa39efc57fae94219
nirenjan <nirenjan@gmail.com>
2021-09-03 10:26:42 -0700

    Add support for Logitech G710+

--- src/devices/keyboard.py
+++ src/devices/keyboard.py
@@ -64,6 +64,48 @@ class Logitech_G910(KeyboardInterface):
         b'\x11\xff\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', # MR release
     }
 
+@dataclass(frozen=True)
+class Logitech_G710p(KeyboardInterface):
+
+    devicename = "Logitech G710+"
+    usbVendor = 0x046d
+    usbProduct = 0xc24d
+    usbInterface = 1
+
+    numMacroKeys = 6
+    numMemoryKeys = 3
+
+    macroKeys = {
+        b'\x03\x01\x00\x00': key.MACRO_1,
+        b'\x03\x02\x00\x00': key.MACRO_2,
+        b'\x03\x04\x00\x00': key.MACRO_3,
+        b'\x03\x08\x00\x00': key.MACRO_4,
+        b'\x03\x10\x00\x00': key.MACRO_5,
+        b'\x03\x20\x00\x00': key.MACRO_6,
+        b'\x02\x08': uinput.KEY_PLAYPAUSE,
+        b'\x02\x04': uinput.KEY_STOP,
+        b'\x02\x02': uinput.KEY_PREVIOUS,
+        b'\x02\x01': uinput.KEY_NEXT,
+        b'\x02\x40': uinput.KEY_MUTE,
+        b'\x02\x10': uinput.KEY_VOLUMEUP,
+        b'\x02\x20': uinput.KEY_VOLUMEDOWN,
+        b'\x03\x00\x80\x00': key.MEMORY_RECORD,
+    }
+
+    memoryKeys = {
+        b'\x03\x00\x10\x00': key.MEMORY_1,
+        b'\x03\x00\x20\x00': key.MEMORY_2,
+        b'\x03\x00\x40\x00': key.MEMORY_3,
+    }
+
+    releaseEvents = {
+        b'\x03\x00\x00\x00', # G release
+        b'\x02\x00', # media key release
+        b'\x03\x00\x00\x00', # M release
+        b'\x03\x00\x00\x00', # MR release
+    }
+
 SUPPORTED_DEVICES = [
-    Logitech_G910
+    Logitech_G910,
+    Logitech_G710p,
 ]
