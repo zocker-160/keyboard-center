bfe99277bbd2521d535f7a186514952592d4a305
zocker-160 <zocker1600@posteo.net>
2021-09-07 17:52:52 +0200

    bump version to 0.1.5

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ TEMPLATE_LOCATION = os.path.join(
 )
 
 PLACEHOLDER_STR = "$$$"
-VERSION = "0.1.4-testing"
+VERSION = "0.1.5-testing"
 
 class AboutWindow(QDialog, Ui_aboutWindow):
     def __init__(self, parent):
