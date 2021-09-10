b50cce130efcfda15e3cb011e6f595d6d5b05db2
zocker-160 <zocker1600@posteo.net>
2021-09-10 20:53:30 +0200

    print in terminal when devmode is enabled

--- src/main.py
+++ src/main.py
@@ -332,6 +332,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 def main():
     app = QApplication(sys.argv)
     devmode = True if "-dev" in sys.argv else False
+    print("Entered DEVMODE")
+    
     if "--version" in sys.argv or "-v" in sys.argv:
         print("version:", VERSION)
         sys.exit()
