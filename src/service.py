ec886525ee2f463f9c20e8b809a139bd31e07b68
zocker-160 <zocker1600@posteo.net>
2021-09-29 23:54:06 +0200

    fix stupid syntax error

--- src/service.py
+++ src/service.py
@@ -73,7 +73,7 @@ def setOpenRGBProfile(profile: str, retry: int, first: bool):
         #    subprocess.Popen(["openrgb", "--server"], shell=False)
         logging.debug("Could not reach OpenRGB SDK, retrying...")
         time.sleep(RETRY_TIMEOUT)
-        setOpenRGBProfile(profile, retry-1)
+        setOpenRGBProfile(profile, retry-1, first)
     except ValueError as e:
         logging.error(str(e))
 
