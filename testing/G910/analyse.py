e5cf7edd2ec29709b8e2c6438a8052c123d7441b
zocker-160 <zocker1600@posteo.net>
2022-03-29 17:41:44 +0200

    add more raw capture data

---
+++ testing/G910/analyse.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import sys
+from scapy.all import (
+    rdpcap
+)
+from scapy.compat import raw
+from scapy.data import EtherDA
+from scapy.packet import Raw
+from scapy.utils import hexdump
+
+packets = rdpcap("exitLGS.pcapng")
+
+#test = Raw()
+#print(raw(packets[36])[:3])
+#print(len(raw(packets[36])))
+
+num = 0
+for i, p in enumerate(packets):
+    if len(raw(p)) == 56:
+    #if len(raw(p)) == 47:
+        #print(num, raw(p)[-20:].hex(), raw(p)[-20:], i+1)
+        print( f"{raw(p)[-20:]}," )
+        num += 1
