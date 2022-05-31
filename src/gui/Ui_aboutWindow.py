3024131f03e74aab30fc2cb824413dfaf1fd7643
zocker-160 <zocker1600@posteo.net>
2022-05-31 20:20:47 +0200

    update year in about window because why not

--- src/gui/Ui_aboutWindow.py
+++ src/gui/Ui_aboutWindow.py
@@ -53,5 +53,5 @@ class Ui_aboutWindow(object):
     def retranslateUi(self, aboutWindow):
         _translate = QtCore.QCoreApplication.translate
         aboutWindow.setWindowTitle(_translate("aboutWindow", "About"))
-        self.about_maintext.setText(_translate("aboutWindow", "<html><head/><body><p>Keyboard Center - $$$</p><p>made by zocker_160</p><p>licensed under GPLv3 | 2021</p><p>source code: <a href=\"https://github.com/zocker-160/keyboard-center\"><span style=\" text-decoration: underline; color:#2980b9;\">https://github.com/zocker-160/keyboard-center</span></a></p></body></html>"))
+        self.about_maintext.setText(_translate("aboutWindow", "<html><head/><body><p>Keyboard Center - $$$</p><p>made by zocker_160</p><p>licensed under GPLv3 | 2021 - 2022</p><p>source code: <a href=\"https://github.com/zocker-160/keyboard-center\"><span style=\" text-decoration: underline; color:#2980b9;\">https://github.com/zocker-160/keyboard-center</span></a></p></body></html>"))
 from . import ressources_rc
