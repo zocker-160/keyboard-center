44a6a216048e13953b873077a5c4fed6661284d1
zocker-160 <zocker1600@posteo.net>
2023-12-02 18:59:17 +0100

    disable silent mode when saving

--- src/mainUi.py
+++ src/mainUi.py
@@ -105,7 +105,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.addKey.clicked.connect(self.addBlankKeyWidget)
         self.addDelay.clicked.connect(self.addBlankDelayWidget)
         self.addCommand.clicked.connect(self.addBlankCommandWidget)
-        self.saveButton.clicked.connect(lambda: self.saveData(True))
+        self.saveButton.clicked.connect(lambda: self.saveData(False))
         self.toTrayButton.clicked.connect(self.hide)
         self.clearAllButton.clicked.connect(
             self.keyListWidgetContents.clearAllEntries)
