373917405c81a60204ddfbf7fe71fedf94b4da0a
zocker-160 <zocker1600@posteo.net>
2024-02-15 13:32:56 +0100

    tag 1.0.7

--- linux_packaging/DEB-keyboard-center/lure.sh
+++ linux_packaging/DEB-keyboard-center/lure.sh
@@ -1,8 +1,8 @@
 maintainer="zocker_160 <zocker1600 at posteo dot net>"
 
 name=keyboard-center
-version=1.0.6
-release=3
+version=1.0.7
+release=1
 desc="Application to map G-keys on (some) Logitech Gaming Keyboards"
 homepage="https://github.com/zocker-160/keyboard-center"
 architectures=('amd64')
