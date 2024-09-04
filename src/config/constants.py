15069ac06eeb114e7a36444628410dcc98cb1d87
zocker-160 <zocker1600@posteo.net>
2024-09-04 20:42:26 +0200

    update APPUUID
    so users can run v1 and v2 at the same time for testing
    not recommended to use in normal everyday use

--- src/config/constants.py
+++ src/config/constants.py
@@ -5,7 +5,7 @@ import os
 APP_NAME = "Keyboard-Center"
 VERSION = "2.0.2"
 
-APPUUID = "0eBJVKjwTanGP7aenveagSRi1c9poja2"
+APPUUID = "73f39707-f330-45b8-9aef-fd7a82149292"
 
 PARENT_LOCATION = os.path.dirname(os.path.abspath(__file__))
 ICON_LOCATION = os.path.join(
