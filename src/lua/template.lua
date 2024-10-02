3171a1c78c823926749475fb700d798104ea0b8b
zocker-160 <zocker1600@posteo.net>
2024-10-02 19:32:11 +0200

    add register functions to lua template documentation

--- src/lua/template.lua
+++ src/lua/template.lua
@@ -5,13 +5,19 @@ Functions Start() and End() are mandatory, validation will fail otherwise!
 
 Internal functions:
 
-KC_sleep(seconds): sleeps for given amount of seconds
+KC_sleep(seconds):        sleeps for given amount of seconds
 KC_sleepMS(milliseconds): sleeps for given amount of milliseconds
-KC_isKeyDown(): returns boolean if key is pressed down or not (can be used in while loops)
-KC_keyClick(keycode): uinput keycode to emit key click
+
+KC_isKeyDown():             returns boolean if key is pressed down or not (can be used in while loops)
+KC_keyClick(keycode):       uinput keycode to emit key click
 KC_keyEmit(keycode, value): uinput keycode to emit; value 1: keydown and 0: keyup
-KC_keySyn(): sync specified keys with uinput (see example scripts)
-KC_logInfo(message): log message on info level
+KC_keySyn():                sync specified keys with uinput (see example scripts)
+
+KC_setGlobalRegister(regsiter, value): sets "register" to "value" (register has to be integer)
+KC_getGlobalRegister(register):        returns value of "register" or 0 if not set
+KC_clearGlobalRegister():              clears all registers
+
+KC_logInfo(message):  log message on info level
 KC_logDebug(message): log message on debug level
 KC_logError(message): log message on error level
 
